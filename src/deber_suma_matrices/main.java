eda428bceb760e447f8f548a9810ba61c1935664
LuisFerVelez <70166432+LuisFerVelez@users.noreply.github.com>
2020-09-08 19:33:08 -0500

    Suma_Matrices

---
+++ src/deber_suma_matrices/main.java
@@ -0,0 +1,17 @@
+
+package deber_suma_matrices;
+
+public class main {
+
+    public static void main(String[] args) {
+        Desarrollo de = new Desarrollo(3,3);
+        de.llenarUno();
+        de.llenarDos();
+        de.resultante();
+        de.mostrarMatrizUno();
+        de.mostraMatrizDos();
+        de.mostrarMatrizTotal();
+        
+    }
+    
+}
